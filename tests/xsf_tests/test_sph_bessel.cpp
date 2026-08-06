3b9fff609b372a6a2c67d6e833259aa1c0cb034f
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2026-08-06 13:43:43 +0200

    TST: missing brace fixed

--- tests/xsf_tests/test_sph_bessel.cpp
+++ tests/xsf_tests/test_sph_bessel.cpp
@@ -317,7 +317,7 @@ TEST_CASE("sqrt complex arm problem", "[spherical_bessel][xsf_tests]") {
     using test_case = std::tuple<std::complex<double>, std::complex<double>, double>;
 
     // Reference values computed with mpmath.
-    auto [z, ref_sqrt, rtol] = GENERATE(test_case{{-5., 0}, {0, 0.56049912163979286, 1e-14});
+    auto [z, ref_sqrt, rtol] = GENERATE(test_case{{-5., 0}, {0, 0.56049912163979286}, 1e-14});
 
     std::complex<double> result_sqrt = std::sqrt(M_PI_2 / z)
     double rel_err_sqrt = xsf::extended_relative_error(result_sqrt, ref_sqrt);
